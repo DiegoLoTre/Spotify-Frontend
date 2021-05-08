f6a8bf9a8bd95da87fb9bab2e93e8dc022caf944
DiegoLoTre <7VeA0bp4QP>
2021-05-08 12:08:48 -0500

    Update header to be dynamic

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navigation-menu">
+    <ul>
+      <li class="nav-item" v-for="item in menu" :key="item.id">
+        <ul v-if="item.subItems">
+
+          <li class="nav-subitem" v-for="subItem in item.subItems" :key="subItem.id">
+            <router-link :to="subItem.href">
+              {{ subItem.title }}
+            </router-link>
+          </li>
+
+        </ul>
+        <router-link v-else :to="item.href">
+          {{ item.title }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      menu: [{
+        title: 'Home',
+        href: '/',
+      }, {
+        title: 'Canciones',
+        subItems: [{
+          title: 'Ver Canciones',
+          href: '/songs',
+        }, {
+          title: 'Agregar Nuevas Canciones',
+          href: '/songs/form',
+        }]
+      }]
+    }
+  }
+}
+</script>
